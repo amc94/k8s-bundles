b67f45c2d3b7c163c9225268c28d9bdec641fb6d
Amjad <amjad.chami@canonical.com>
2025-06-24 09:47:18 -0400

    some missing changes

--- terraform/applications.tf
+++ terraform/applications.tf
@@ -2,7 +2,7 @@
 # See LICENSE file for licensing details.
 
 locals {
-  _k8s_keys      = keys(module.k8s_config.config)
+  _k8s_keys      = keys(var.k8s_config.config)
   _k8s_key_count = length(local._k8s_keys)
   k8s_config     = local._k8s_key_count == 1 ? module.k8s_config.config[local._k8s_keys[0]] : null
 }
@@ -21,8 +21,8 @@ resource "null_resource" "validate_unique_k8s" {
 output "debug" {
   value = {
     k8s_config = var.k8s_config
-    ceph       = [for ceph in module.ceph : ceph.debug]
-    openstack  = [for openstack in module.openstack : openstack.debug]
+    ceph       = [for ceph in var.ceph : ceph.debug]
+    #openstack  = [for openstack in module.openstack : openstack.debug]
   }
 }
 
@@ -50,7 +50,7 @@ module "k8s" {
 
 module "k8s_worker" {
   source      = "git::https://github.com/canonical/k8s-operator//charms/worker/terraform?ref=main"
-  for_each    = module.k8s_worker_config.config
+  for_each    = var.k8s_worker_config.config
   app_name    = each.value.app_name
   base        = coalesce(each.value.base, local.k8s_config.base)
   constraints = coalesce(each.value.constraints, local.k8s_config.constraints)
@@ -62,19 +62,19 @@ module "k8s_worker" {
   units       = each.value.units
 }
 
-module "openstack" {
-  count  = var.cloud_integration == "openstack" ? 1 : 0
-  source = "./openstack"
-  model  = resource.juju_model.this.name
-  k8s = {
-    app_name    = module.k8s.app_name
-    base        = var.k8s_config.base
-    constraints = var.k8s_config.constraints
-    channel     = var.k8s_config.channel
-    provides    = module.k8s.provides
-    requires    = module.k8s.requires
-  }
-}
+#module "openstack" {
+#  count  = var.cloud_integration == "openstack" ? 1 : 0
+#  source = "./openstack"
+#  model  = resource.juju_model.this.name
+#  k8s = {
+#    app_name    = module.k8s.app_name
+#    base        = var.k8s_config.base
+#    constraints = var.k8s_config.constraints
+#    channel     = var.k8s_config.channel
+#    provides    = module.k8s.provides
+#    requires    = module.k8s.requires
+#  }
+#}
 
 module "ceph_csi" {
   count       = length(var.csi_integration) > 0 ? 1 : 0
