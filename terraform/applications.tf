fcfead19e8553690d81dfbcb8cbaecd9e6b6c795
Amjad <amjad.chami@canonical.com>
2025-06-26 14:36:26 -0400

    add grafana agents

--- terraform/applications.tf
+++ terraform/applications.tf
@@ -89,3 +89,14 @@ module "ceph_csi" {
   revision = var.csi_config.revision
 }
 
+module "grafana_agent" {
+  count       = length(var.cos_endpoints) > 0 ? 1 : 0
+  source      = "git::https://github.com/canonical/grafana-agent-operator/tree/main/terraform?ref-main"
+  model_name  = var.model_name
+  app_name    = var.grafana_agent_config.app_name
+  channel     = var.grafana_agent_config.channel
+  config      = var.grafana_agent_config.config
+  constraints = var.grafana_agent_config.constraints
+  revision    = var.grafana_agent_config.revision
+  units       = var.grafana_agent_config.units
+}
